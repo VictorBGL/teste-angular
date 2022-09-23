a2f9a8f1c6871c35264aa8af7b28e69a775ab0bd
VictorBGL <109552034+VictorBGL@users.noreply.github.com>
2022-09-23 16:40:43 -0300

    commit

--- apps/teste-angular/src/app/inicio/inicio.routing.ts
+++ apps/teste-angular/src/app/inicio/inicio.routing.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio.component';
 
 
-const routes: Routes = [
+const routes: Routes =  [
   {
     path: '',
     component: InicioComponent,
